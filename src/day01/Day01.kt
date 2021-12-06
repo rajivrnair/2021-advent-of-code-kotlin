415f6447cc61a170441ff1e67eef8517aaea1991
Rajiv Nair <rajivrnair@gmail.com>
2021-12-06 17:31:10 +0530

    Day 1

---
+++ src/day01/Day01.kt
@@ -0,0 +1,22 @@
+package day01
+
+import readInputAsInt
+
+fun main() {
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { (a,b) -> a < b }
+    }
+
+    fun part2(input: List<Int>): Int {
+        return input.windowed(4).count { it[0] < it[3] }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInputAsInt("Day01_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
+
+    val input = readInputAsInt("Day01")
+    println(part1(input))
+    println(part2(input))
+}
